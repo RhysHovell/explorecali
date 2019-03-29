8e218e38471291f25d1c9166b286df48f9114524
hovelr <rhyshovell@gmail.com>
2019-03-29 16:40:45 +0000

    Starting out

--- src/main/java/com/example/ec/domain/Region.java
+++ src/main/java/com/example/ec/domain/Region.java
@@ -1,4 +1,15 @@
 package com.example.ec.domain;
 
 public enum Region {
+    Central_Coast("Central_Coast"), Southern_California("Southern_California"),Northern_California("Northern_California"),Varies("Varies");
+    private String label;
+    Region(String label) {this.label = label;}
+    public static Region findByLabel(String byLabel) {
+        for(Region r:Region.values()) {
+            if (r.label.equalsIgnoreCase(byLabel))
+                return r;
+
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/ec/domain/Tour.java
+++ src/main/java/com/example/ec/domain/Tour.java
@@ -1,7 +1,74 @@
 package com.example.ec.domain;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 public class Tour {
+
+    @Id
+    @GeneratedValue
+    @Column
+    @Getter
+    private Integer id;
+
+    @Column
+    @Getter @Setter
+    private String title;
+
+    @Column
+    @Getter @Setter
+    private String description;
+
+    @Column
+    @Getter @Setter
+    private String blurb;
+
+    @Column
+    @Getter @Setter
+    private Integer price;
+
+    @Column
+    @Getter @Setter
+    private String duration;
+
+    @Column
+    @Getter @Setter
+    private String bullets;
+
+    @Column
+    @Getter @Setter
+    private String keywords;
+
+    @ManyToOne
+    private TourPackage tourPackage;
+
+    @Column
+    private Difficulty difficulty;
+
+    @Column
+    private Region region;
+
+    public Tour(String title,String description, String blurb, Integer price, String duration, String bullets,
+                String keywords, TourPackage tourPackage, Difficulty difficulty, Region region) {
+        this.title = title;
+        this.description=description;
+        this.blurb=blurb;
+        this.price=price;
+        this.duration=duration;
+        this.bullets=bullets;
+        this.keywords=keywords;
+        this.tourPackage=tourPackage;
+        this.difficulty=difficulty;
+        this.region=region;
+    }
+
+    protected Tour() {
+
+    }
+
+
+
 }
